15595a1cc4e9860973530bfb98de717a0481011e
DaMoN <baironvargas0@gmail.com>
2025-06-25 16:11:31 -0600

    Agrega firebaseConfig usando variables de entorno

--- src/firebaseConfig.ts
+++ src/firebaseConfig.ts
@@ -1,15 +1,14 @@
-// Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
 import { getFirestore } from 'firebase/firestore';
 
 const firebaseConfig = {
-  apiKey: "process.env.EXPO_PUBLIC_FIREBASE_API_KEY",
-  authDomain: "sinpe-sodapatriarca.firebaseapp.com",
-  projectId: "sinpe-sodapatriarca",
-  storageBucket: "sinpe-sodapatriarca.firebasestorage.app",
-  messagingSenderId: "179036112046",
-  appId: "1:179036112046:web:d9c0eac0a63700c956193f",
-  measurementId: "G-WEB2F6RWYZ"
+  apiKey: process.env.EXPO_PUBLIC_FIREBASE_API_KEY,
+  authDomain: process.env.EXPO_PUBLIC_FIREBASE_AUTH_DOMAIN,
+  projectId: process.env.EXPO_PUBLIC_FIREBASE_PROJECT_ID,
+  storageBucket: process.env.EXPO_PUBLIC_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.EXPO_PUBLIC_FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.EXPO_PUBLIC_FIREBASE_APP_ID,
+  measurementId: process.env.EXPO_PUBLIC_FIREBASE_MEASUREMENT_ID
 };
 
 const app = initializeApp(firebaseConfig);
