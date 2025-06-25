d831563fa85b2aab1f8f01e061cbae63880e8e0f
DaMoN <baironvargas0@gmail.com>
2025-06-25 16:16:46 -0600

    Resuelto conflicto en firebaseConfig.ts

--- src/firebaseConfig.ts
+++ src/firebaseConfig.ts
@@ -1,12 +1,7 @@
-<<<<<<< HEAD
-=======
-// Import the functions you need from the SDKs you need
->>>>>>> b0e10e61acfebbeb3a6db02e3232dbe1266f6dc9
 import { initializeApp } from "firebase/app";
 import { getFirestore } from 'firebase/firestore';
 
 const firebaseConfig = {
-<<<<<<< HEAD
   apiKey: process.env.EXPO_PUBLIC_FIREBASE_API_KEY,
   authDomain: process.env.EXPO_PUBLIC_FIREBASE_AUTH_DOMAIN,
   projectId: process.env.EXPO_PUBLIC_FIREBASE_PROJECT_ID,
@@ -18,16 +13,3 @@ const firebaseConfig = {
 
 const app = initializeApp(firebaseConfig);
 export const db = getFirestore(app);
-=======
-  apiKey: "process.env.EXPO_PUBLIC_FIREBASE_API_KEY",
-  authDomain: "sinpe-sodapatriarca.firebaseapp.com",
-  projectId: "sinpe-sodapatriarca",
-  storageBucket: "sinpe-sodapatriarca.firebasestorage.app",
-  messagingSenderId: "179036112046",
-  appId: "1:179036112046:web:d9c0eac0a63700c956193f",
-  measurementId: "G-WEB2F6RWYZ"
-};
-
-const app = initializeApp(firebaseConfig);
-export const db = getFirestore(app);
->>>>>>> b0e10e61acfebbeb3a6db02e3232dbe1266f6dc9
